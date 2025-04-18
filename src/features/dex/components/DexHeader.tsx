b941eb885e0c5e055e4a861b3c7f7d60aba07197
dxmlk <1101sky@korea.ac.kr>
2025-04-18 19:33:13 +0900

    refactor: dex 최신 디자인으로 수정

--- src/features/dex/components/DexHeader.tsx
+++ src/features/dex/components/DexHeader.tsx
@@ -1,7 +1,7 @@
 import { ReactComponent as  ScrapIcon } from 'assets/icons/scrap.svg';
 import { ReactComponent as  SearchIcon } from 'assets/icons/search.svg';
 import { ReactComponent as  CalendarIcon } from 'assets/icons/calendar.svg';
-import { ReactComponent as  LeafIcon } from 'assets/icons/leaf.svg';
+import { ReactComponent as  HabitatIcon } from 'assets/icons/habitat.svg';
 import { ReactComponent as  AaIcon } from 'assets/icons/aa.svg';
 import { useState } from 'react';
 
@@ -9,39 +9,50 @@ import { useState } from 'react';
 const DexHeader = () => {
     const [activeFilter, setActiveFilter] = useState<string | null>(null);
     
+    // 나중에 유저 id도 받아야 돼 유저마다 스크랩 다르니까... 
+    // const { id } = useParams(); 
+    // navigate(`/${id}/scrap`);
+    // const navigate = useNavigate();
+
     const handleFilterClick = (filterName: string) => {
         setActiveFilter(prev => (prev === filterName ? null : filterName));
     };
+    const handleSearchClick = () => {
+
+    };
 
     return (
-        <div className='bg-white font-pretendard flex flex-col justify-between h-[106px] px-[24px]'>
-            <div className='mt-[24px] flex flex-row justify-between'>
+        <div className='bg-white font-pretendard flex flex-col justify-between h-[114px] px-[24px]'>
+            <div className='mt-[17px] flex flex-row justify-between'>
                 <span className='text-black font-700 text-[22px]'>도감</span>
-                <div className='flex flex-row gap-[30px]'>
-                    <ScrapIcon className='w-[14px] h-[21px]'/>
-                    <SearchIcon className='w-[18.28px] h-[18.28px]'/>
+                <div className='flex flex-row gap-[31px]'>
+                    <button onClick={() => handleFilterClick('스크랩')}>
+                        <ScrapIcon className={`h-[21px] ${activeFilter === '스크랩' ? 'fill-green stroke-green' : 'fill-white stroke-black '}`}/></button>
+                    <button onClick={handleSearchClick}><SearchIcon className='h-[18.28px]'/></button>
                 </div>
             </div>
-            <div className='mb-[9px] flex gap-[10px]'>
-                <button className={`shadow flex flex-row text-[13.536px] w-[65px] h-[30px] rounded-[90.238px] justify-center items-center gap-[6px] ${activeFilter === '계절' ? 'bg-green text-[#fefefe] font-700 border-green': 'border text-[#0d0d0d] font-400 border-[#C6C6C6] bg-[#FEFEFE]'}`}
+            <div className='mb-[12px] flex gap-[12px]'>
+                <button className={`gap-[5px] flex flex-row text-[15px] w-[70px] h-[33px] rounded-[100px] justify-center items-center ${activeFilter === '계절' ? 'bg-green text-[#fefefe] font-700': 'text-[#0d0d0d] font-400 bg-[#f2f2f2]'}`}
                         onClick={() => handleFilterClick('계절')}>
-                    <CalendarIcon className={`w-[13px] h-[13px] ${activeFilter === '계절' ? 'stroke-white' : 'stroke-black'}`}/>
+                    <CalendarIcon className={`w-[13px] h-[13px] ${activeFilter === '계절' ? 'fill-white' : 'fill-black'}`}/>
                     <span>계절</span>
                 </button>
-                <button className={`shadow flex flex-row text-[13.536px] w-[75px] h-[30px] rounded-[90.238px] justify-center items-center gap-[6px] ${activeFilter === '서식지' ? 'bg-green text-[#fefefe] font-700 border-green': 'border text-[#0d0d0d] font-400 border-[#C6C6C6] bg-[#FEFEFE]'}`}
+                <button className={`gap-[5px] flex flex-row text-[15px] w-[83px] h-[33px] rounded-[100px] justify-center items-center ${activeFilter === '서식지' ? 'bg-green text-[#fefefe] font-700': 'text-[#0d0d0d] font-400 bg-[#f2f2f2]'}`}
                         onClick={() => handleFilterClick('서식지')}>
-                    <LeafIcon className={`w-[12px] h-[12px] ${activeFilter === '서식지' ? 'stroke-white' : 'stroke-black'}`}/>
+                    <HabitatIcon className={`w-[13px] h-[11px] ${activeFilter === '서식지' ? 'stroke-white fill-white ' : 'fill-black'}`}/>
                     <span>서식지</span>
                 </button>                
-                <button className={`shadow flex flex-row text-[13.536px] w-[65px] h-[30px] rounded-[90.238px] justify-center items-center gap-[6px] ${activeFilter === '크기' ? 'bg-green text-[#fefefe] font-700 border-green': 'border text-[#0d0d0d] font-400 border-[#C6C6C6] bg-[#FEFEFE]'}`}
+                <button className={`gap-[5px] flex flex-row text-[15px] w-[72px] h-[33px] rounded-[100px] justify-center items-center ${activeFilter === '크기' ? 'bg-green text-[#fefefe] font-700 ': 'text-[#0d0d0d] font-400 bg-[#f2f2f2]'}`}
                         onClick={() => handleFilterClick('크기')}>
-                    <AaIcon className={`w-[16px] h-[14px] ${activeFilter === '크기' ? 'stroke-white' : 'stroke-black'}`}/>
+                    <AaIcon className={`w-[15px] h-[10px] ${activeFilter === '크기' ? 'stroke-white ' : 'fill-black'}`}/>
                     <span>크기</span>
                 </button>
             </div>
         </div>
     )
 }
+
+// 이거 아이콘 컴포넌트 디자인인 수정 필요
 
 export default DexHeader;
 
--- src/features/dex/components/DexList.tsx
+++ src/features/dex/components/DexList.tsx
@@ -1,3 +1,5 @@
+import { ReactComponent as ScrapIcon } from "assets/icons/scrap.svg";
+
 interface DexItemType {
     id: number;
     nameKor: string;
@@ -13,13 +15,13 @@ interface DexListProps {
 const DexList = ({dexItems}: DexListProps ) => {
     return (
         <>
-            
+
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[15px]'>
                 {dexItems.map((item) => (
-                    <div key={item.id} className='rounded-[5px] border border-[#D9D9D9] bg-[#FEFEFE] flex flex-col w-full h-[198px] overflow-hidden transform transition-transform duration-300 hover:hover:scale-105'>
+                    <div key={item.id} className='rounded-[10px] border border-[1px] border-[#D9D9D9] bg-[#FEFEFE] flex flex-col w-full h-[198px] overflow-hidden transform transition-transform duration-300 hover:hover:scale-105'>
                         <img src={item.birdImg} alt={item.nameKor} className='w-full h-[142px] object-cover'/>
-                        <span className='ml-[11.45px] mt-[10px] text-[#000000] text-[15px] font-pretendard font-600'>{item.nameKor}</span>
-                        <span className='ml-[11.45px] mb-[10px] text-[#979797] text-[13px] font-pretendard font-400'>{item.nameEng}</span>
+                        <span className='mx-[11px] mt-[10px] font-pretendard flex flex-col text-[#000000] text-[15px] font-600'>{item.nameKor}</span>
+                        <span className='mx-[11px] font-pretendard flex flex-col text-[#979797] text-[13px] font-400'>{item.nameEng}</span>
                     </div>  
                 ))}
 
