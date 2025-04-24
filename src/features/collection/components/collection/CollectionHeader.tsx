1c309d85ed20b8aa89c7d30368808a903628f674
dxmlk <1101sky@korea.ac.kr>
2025-04-24 19:26:11 +0900

    feat: CollectionPage 정렬 바텀시트 구현

--- src/features/collection/components/collection/CollectionHeader.tsx
+++ src/features/collection/components/collection/CollectionHeader.tsx
@@ -1,27 +1,37 @@
-import { ReactComponent as  SearchIcon } from 'assets/icons/search.svg';
+import { ReactComponent as SearchIcon } from "assets/icons/search.svg";
+import { ReactComponent as SortIcon } from "assets/icons/sort.svg";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-
+import SortBottomSheet from "./SortBottomSheet";
 
 const CollectionHeader = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
 
-    function handleEditClick(): void {
-        throw new Error("Function not implemented.");
-    }
+  const handleSortClick = () => {
+    setIsBottomSheetOpen(true);
+  };
+  const closeBottomSheet = () => {
+    setIsBottomSheetOpen(false);
+  };
 
-    const handleSearchClick = () => {
-        navigate(`/search`);
-    }
+  const handleSearchClick = () => {
+    navigate(`/search`);
+  };
 
-    return (
-        <div className='h-[66px] px-[24px] items-center flex flex-row justify-between bg-white font-pretendard'>
-            <span className='text-black font-700 text-[22px]'>컬렉션</span>
-            <div className='text-[#0d0d0d] font--400 text-[18px] flex flex-row gap-[18px]'>
-                <button onClick={() => handleEditClick()}>편집</button>
-                <button onClick={handleSearchClick}><SearchIcon className='h-[18.28px] text-[#0d0d0d]'/></button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="h-[66px] px-[24px] items-center flex flex-row justify-between bg-white font-pretendard">
+      <span className="text-black font-700 text-[22px]">컬렉션</span>
+      <div className="text-[#0d0d0d] font--400 text-[18px] flex flex-row gap-[18px]">
+        <button onClick={() => handleSortClick()}>
+          <SortIcon className="w-[4px]" />
+        </button>
+        <button onClick={handleSearchClick}>
+          <SearchIcon className="h-[18.28px] text-[#0d0d0d]" />
+        </button>
+      </div>
+      <SortBottomSheet isOpen={isBottomSheetOpen} onClose={closeBottomSheet} />
+    </div>
+  );
+};
 export default CollectionHeader;
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,12 +1,9 @@
 import SearchBar from "components/common/SearchBar";
+import SortBottomSheet from "features/collection/components/collection/SortBottomSheet";
+import SortBottomSheeet from "features/collection/components/collection/SortBottomSheet";
 
 const Test = () => {
-  return (
-    <>
-
-
-    </>
-  )
+  return <></>;
 };
 
 export default Test;
