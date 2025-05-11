d9ded69cf94ac95e5a3bd077f7bae511bb73a6dd
dxmlk <1101sky@korea.ac.kr>
2025-05-11 16:56:52 +0900

    refactor: 버튼/내비게이션   바 겹침 수정

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -10,6 +10,8 @@ const Layout = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const hideNavBar = ["/add-collection"].some((path) => location.pathname.startsWith(path));
+
   const isActive = (path: string) => location.pathname.startsWith(path);
 
   return (
@@ -18,43 +20,45 @@ const Layout = () => {
         <div className="flex-grow">
           <Outlet />
         </div>
-        <nav
-          className=" rounded-t-[20px] border-none fixed bottom-0 left-0 right-0 mx-auto flex h-60 max-w-[500px] items-center bg-white"
-          style={{ boxShadow: "0 -4px 12px rgba(0, 0, 0, 0.1)" }}
-        >
-          <button className="flex-1 flex flex-col items-center text-[#6D6D6D]">
-            <CommunityIcon className="w-[19px] h-[19px]" />
-            <div className="text-[13px] mt-[7px]">커뮤니티</div>
-          </button>
-          <button
-            onClick={() => navigate("/dex")}
-            className={`flex-1 flex flex-col items-center ${isActive("/dex") ? "text-green" : "text-[#6d6d6d]"}`}
+        {!hideNavBar && (
+          <nav
+            className="h-[96px] rounded-t-[20px] border-none fixed bottom-0 left-0 right-0 mx-auto flex h-60 max-w-[500px] items-center bg-white"
+            style={{ boxShadow: "0 -4px 12px rgba(0, 0, 0, 0.1)" }}
           >
-            <DexIcon className="w-[22px] h-[19px]" />
-            <div className="text-[13px] mt-[7px]">도감</div>
-          </button>
-          <button
-            onClick={() => navigate("/map")}
-            className={`flex-1 flex flex-col items-center ${isActive("/map") ? "text-green" : "text-[#6d6d6d]"}`}
-          >
-            <MapIcon className="w-[20px] h-[20px]" />
-            <div className="text-[13px] mt-[7px]">지도</div>
-          </button>
-          <button
-            onClick={() => navigate("/collection")}
-            className={`flex-1 flex flex-col items-center ${isActive("/collection") ? "text-green" : "text-[#6d6d6d]"}`}
-          >
-            <SaerokIcon className="w-[22px] h-[20px]" />
-            <div className="text-[13px] mt-[7px]">새록</div>
-          </button>
-          <button
-            onClick={() => navigate("/my-page")}
-            className={`flex-1 flex flex-col items-center ${isActive("/my-page") ? "text-green" : "text-[#6d6d6d]"}`}
-          >
-            <MyIcon className="w-[17px] h-[20px]" />
-            <div className="text-[13px] mt-[7px]">MY</div>
-          </button>
-        </nav>
+            <button className="flex-1 flex flex-col items-center text-[#6D6D6D]">
+              <CommunityIcon className="w-[19px] h-[19px]" />
+              <div className="text-[13px] mt-[7px]">커뮤니티</div>
+            </button>
+            <button
+              onClick={() => navigate("/dex")}
+              className={`flex-1 flex flex-col items-center ${isActive("/dex") ? "text-green" : "text-[#6d6d6d]"}`}
+            >
+              <DexIcon className="w-[22px] h-[19px]" />
+              <div className="text-[13px] mt-[7px]">도감</div>
+            </button>
+            <button
+              onClick={() => navigate("/map")}
+              className={`flex-1 flex flex-col items-center ${isActive("/map") ? "text-green" : "text-[#6d6d6d]"}`}
+            >
+              <MapIcon className="w-[20px] h-[20px]" />
+              <div className="text-[13px] mt-[7px]">지도</div>
+            </button>
+            <button
+              onClick={() => navigate("/collection")}
+              className={`flex-1 flex flex-col items-center ${isActive("/collection") ? "text-green" : "text-[#6d6d6d]"}`}
+            >
+              <SaerokIcon className="w-[22px] h-[20px]" />
+              <div className="text-[13px] mt-[7px]">새록</div>
+            </button>
+            <button
+              onClick={() => navigate("/my-page")}
+              className={`flex-1 flex flex-col items-center ${isActive("/my-page") ? "text-green" : "text-[#6d6d6d]"}`}
+            >
+              <MyIcon className="w-[17px] h-[20px]" />
+              <div className="text-[13px] mt-[7px]">MY</div>
+            </button>
+          </nav>
+        )}
       </div>
     </div>
   );
